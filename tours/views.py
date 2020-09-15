7a138ba2cc66de523810359b1fcc3663db676b10
Konstantin <ponomarevkonst@gmail.com>
2020-09-16 00:53:09 +0300

    Initial commit

---
+++ tours/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpResponseNotFound
+
+
+class MainView(TemplateView):
+    template_name = 'index.html'
+
+
+class DepartureView(TemplateView):
+    template_name = 'departure.html'
+
+
+class TourView(TemplateView):
+    template_name = 'tour.html'
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('404')
+
+
+def custom_handler500(request, *args):
+    return HttpResponseNotFound('500')
